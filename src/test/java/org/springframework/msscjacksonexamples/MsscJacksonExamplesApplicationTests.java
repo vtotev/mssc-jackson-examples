14b103cdde80932e7d4c39a109c04226adce082a
Valeri Totev <72451048+vtotev@users.noreply.github.com>
2023-12-07 17:25:11 +0200

    initial commit

---
+++ src/test/java/org/springframework/msscjacksonexamples/MsscJacksonExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springframework.msscjacksonexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscJacksonExamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
